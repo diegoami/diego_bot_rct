ff62d4a729c9e1dd2c5408395301cb371ad0df08
Diego Amicabile <diego.amicabile@gmail.com>
2018-04-15 23:13:59 +0200

    At last it works somehow.

--- src/Conversation.js
+++ src/Conversation.js
@@ -13,24 +13,34 @@ class Conversation extends Component {
             conversation_lines : [],
             inputfield : ""
         };
-        this.handleClick  = this.handleClick.bind(this);
-        this.updateInputValue = this.updateInputValue.bind(this);
-
+        this.endpoint = process.env.REACT_APP_RASA_ENDPOINT
+        this.handleClick  = this.handleClick.bind(this)
+        this.updateInputValue = this.updateInputValue.bind(this)
     }
 
     handleClick = e => {
-        console.log(this.state.inputfield)
-        this.setState(prevState => ({
-            conversation_lines : [...prevState.conversation_lines, prevState.inputfield]
-        }))
-        console.log(this.state.conversation_lines)
         e.preventDefault()
+        let endpoint = this.endpoint+this.props.id+'/respond'
+        console.log(endpoint)
+        fetch(endpoint, {
+            method: 'POST',
+            headers: {
+            },
+            body: JSON.stringify({
+                'query': this.state.inputfield,
+            })
+        }).then(res => res.json())
+           .then(data => {
+                console.log(data)
+                var text = data[0]["text"]
+                this.setState(prevState => ({
+                    conversation_lines : [...prevState.conversation_lines, { "question" : prevState.inputfield, "answer" : text } ]
+                }))
+                console.log(this.state.conversation_lines)
+            })
     }
 
     updateInputValue = evt => {
-        console.log(evt)
-        console.log(evt.target)
-        console.log(evt.target.value)
         let evalue = evt.target.value
         this.setState(prevState =>
             ({
@@ -45,10 +55,15 @@ class Conversation extends Component {
         return (
           <div className="conversation">
               { this.state.conversation_lines.map(function(conversation_line, index) {
-                  return <p className="conversation_line"  > { conversation_line } </p>
+                  return (
+                    <div>
+                      <p className="question"  >  { conversation_line["question"] } </p>
+                      <p className="answer"  >    { conversation_line["answer"] } </p>
+                    </div>
+                  )
               })}
-              <Form>
-                  <Label for="conv_line_input">Conversation Line</Label>
+              <Form className="conv_form">
+                  <Label for="conv_line_input">Type your message</Label>
                   <Input name="conv_line" id="conv_line_input" type="text" onChange={this.updateInputValue}   />
                   <Button id="add_conversation_button"  onClick={this.handleClick} type="submit">Submit</Button>
               </Form>
