6750c6952c2d3bee836914e1ab5e5367ed17e267
Diego Amicabile <diego.amicabile@gmail.com>
2018-04-16 23:40:33 +0200

    Better layout

--- src/Conversation.js
+++ src/Conversation.js
@@ -22,25 +22,29 @@ class Conversation extends Component {
         e.preventDefault()
         let endpoint = this.endpoint+this.props.id+'/respond'
         console.log(endpoint)
-        fetch(endpoint, {
-            method: 'POST',
-            headers: {
-            },
-            body: JSON.stringify({
-                'query': this.state.inputfield,
+        if (this.state.inputfield.length > 0 ) {
+            fetch(endpoint, {
+                method: 'POST',
+                headers: {},
+                body: JSON.stringify({
+                    'query': this.state.inputfield,
+                })
+            }).then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    var text = data[0]["text"]
+                    this.setState(prevState => ({
+                        conversation_lines: [{
+                            "question": prevState.inputfield,
+                            "answer": text
+                        }, ...prevState.conversation_lines,],
+                        inputfield: ""
+                    }))
+                    console.log(this.state.conversation_lines)
+
+
             })
-        }).then(res => res.json())
-           .then(data => {
-                console.log(data)
-                var text = data[0]["text"]
-                this.setState(prevState => ({
-                    conversation_lines : [...prevState.conversation_lines, { "question" : prevState.inputfield, "answer" : text } ],
-                    inputfield : ""
-                }))
-                console.log(this.state.conversation_lines)
-
-
-           })
+        }
     }
 
     updateInputValue = evt => {
@@ -57,6 +61,12 @@ class Conversation extends Component {
     render() {
         return (
           <div className="conversation">
+              <Form className="enter_question">
+                  <Label for="conv_line_input">Hi, I am Diego-bot ! Type your message here.</Label>
+                  <Input name="conv_line" id="conv_line_input" type="text" value={this.state.inputfield}  onChange={this.updateInputValue}   />
+                  <Button id="add_conversation_button"  onClick={this.handleClick} type="submit">Submit</Button>
+              </Form>
+              <div className = "conv_history">
               { this.state.conversation_lines.map(function(conversation_line, index) {
                   return (
                     <div>
@@ -65,11 +75,7 @@ class Conversation extends Component {
                     </div>
                   )
               })}
-              <Form className="conv_form">
-                  <Label for="conv_line_input">Type your message</Label>
-                  <Input name="conv_line" id="conv_line_input" type="text" value={this.state.inputfield}  onChange={this.updateInputValue}   />
-                  <Button id="add_conversation_button"  onClick={this.handleClick} type="submit">Submit</Button>
-              </Form>
+              </div>
 
           </div>
     );
